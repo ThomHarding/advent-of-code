970bbf356e3219cfae06e968d9db63647784c5f1
THOMAS HARDING <hardingt604@gmail.com>
2023-01-03 23:55:23 -0600

    day 11 done. modular arithmetic makes my brain wrinkle

--- week2/day11.js
+++ week2/day11.js
@@ -1,6 +1,4 @@
-const { match } = require('assert');
 const {readFileSync, promises =  fsPromises} = require('fs');
-const { type } = require('os');
 
 function syncReadFile(filename) {
   const contents = readFileSync(filename, 'utf-8');
@@ -29,22 +27,33 @@ for (let i = 0; i < file.length; i+=7) {
 
 function operate(operation, item) {
    return eval(operation.replace(/old/g, item))
+   //monkey.operation is a string that spells out the math we want and eval() lets us run that string as a math operation
 }
 
 function inspectItem(monkey, itemValue) {
-    let item = Math.floor(operate(monkey.operation, itemValue) / 3);
+    let all_divisors = monkeys.map((monkey) => monkey.isDivisibleBy).reduce((a, b) => a * b, 1);
+    //this line makes my brain hurt. thank you advent of code reddit and u/heyitsmattwade in particular
+    //so a couple thousand rounds in, the numbers start getting too large to calculate and the divisible by math breaks down.
+    //heyitsmattwade breaks it down better but effectively, we know that a % n = a at some point early on we want to shrink numbers.
+    //a is our current item value and n is the lowest common multiple of all our items. (I think. I might be wrong)
+    //despite being equivalent, a % n is a much smaller number than just a and we can save just the remainder, the math still works out
+    //we just do this 'modular arithmetic' to every 'congruent relation' (scare quotes intentional) every time we look at an item to keep item values low.
+    //the end result is that every time we inspect an item, we run item value = item value mod lowest common multiple and it lowers it to reasonable levels
+    let item = Math.floor(operate(monkey.operation, itemValue)); //divide by 3 for part 1 isntead of this 35 and 44
+    item = item % all_divisors;
     if ((item % monkey.isDivisibleBy) === 0) {
+        //beyond this, just pass to trueMonkey if itemvalue is divisble by the monkey's divisibleBy score, and pass to falseMonkey if it isn't
         monkeys[monkey.isTrueMonkey].items.push(item);
     } else {
         monkeys[monkey.isFalseMonkey].items.push(item);
     }
 }
 
-for (let i = 0; i < 20; i++) { //i < 20
-    for (let j = 0; j < monkeys.length; j++) {
+for (let i = 0; i < 10000; i++) { //i < 20 for p1
+    for (let j = 0; j < monkeys.length; j++) { //check every item of every monkey
         for (let t = 0; t < monkeys[j].items.length; t++) {
-            monkeys[j].itemsInspected++;
-            inspectItem(monkeys[j], monkeys[j].items[t]);
+            monkeys[j].itemsInspected++; //count every time a monkey inspects for answer purposes
+            inspectItem(monkeys[j], monkeys[j].items[t]); //the big math
         }
         monkeys[j].items = []; //monkey will always throw away all his items midround but i don't want to change the list while going through it
     }
